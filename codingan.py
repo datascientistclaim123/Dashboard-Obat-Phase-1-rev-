0852ac8930df92553d9c930ae70d49f1480764d0
datascientistclaim123 <datascientistclaim@gmail.com>
2024-11-29 14:30:07 +0700

    Update codingan.py

--- codingan.py
+++ codingan.py
@@ -6,17 +6,17 @@ import matplotlib.pyplot as plt
 # Fungsi untuk menghitung median berbobot berdasarkan kolom Qty
 def weighted_median(group):
 
-    group["HargaSatuan"] = group["HargaSatuan"].fillna(0)
+    group["Harga Satuan"] = group["Harga Satuan"].fillna(0)
     
     # Urutkan data berdasarkan Harga Satuan
-    group = group.sort_values("HargaSatuan").reset_index()
+    group = group.sort_values("Harga Satuan").reset_index()
     # Hitung jumlah kumulatif Qty
     group["CumulativeQty"] = group["Qty"].cumsum()
     # Cari total median pada kolom Qty
     median_qty = group["Qty"].sum() / 2
     # Cari baris di mana cumulative Qty melewati median
     median_row = group[group["CumulativeQty"] >= median_qty].iloc[0]
-    return median_row["HargaSatuan"]
+    return median_row["Harga Satuan"]
 
 # Cache untuk membaca dataset
 @st.cache_data
